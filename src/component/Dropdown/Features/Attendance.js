326fbed37383c405ab54407a60fadaf775c53e41
ismailpasha <116712252+ismailpasha@users.noreply.github.com>
2023-01-04 23:11:00 +0530

    navbar update it

--- src/component/Dropdown/Features/Attendance.js
+++ src/component/Dropdown/Features/Attendance.js
@@ -10,7 +10,7 @@ export const Attendance = () => {
           <div className="container py-5 h-100">
             <div className="row d-flex justify-content-center align-items-center h-100">
               <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                <div className="card bg-dark text-white" style={{borderRadius: "1rem"}}>
+                <div className="card bg-dark text-white" style={{ borderRadius: "1rem" }}>
                   <div className="card-body p-5 text-center">
 
                     <div className="mb-md-5 mt-md-4 pb-5">
@@ -19,13 +19,15 @@ export const Attendance = () => {
                       <p className="text-white-50 mb-5">Please enter your login and password!</p>
 
                       <div className="form-outline form-white mb-4">
-                        <input type="email" id="typeEmailX" className="form-control form-control-lg" />
                         <label className="form-label" htmlFor="typeEmailX">Email</label>
+                        <input type="email" id="typeEmailX" className="form-control form-control-lg" />
+
                       </div>
 
                       <div className="form-outline form-white mb-4">
-                        <input type="password" id="typePasswordX" className="form-control form-control-lg" />
                         <label className="form-label" htmlFor="typePasswordX">Password</label>
+                        <input type="password" id="typePasswordX" className="form-control form-control-lg" />
+
                       </div>
 
                       {/* <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p> */}

735e5f9e19998cedbabeca77a0c3ba8cd81c43e0
V. M. Shahbaaz Khan <khanvms9@gmail.com>
2023-05-15 00:23:18 +0530

    upstream merge complete

--- src/component/Common/Navbar.js
+++ src/component/Common/Navbar.js
@@ -28,35 +28,19 @@ const Navbar = () => {
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
 
-<<<<<<< Updated upstream
                   <NavLink activeClassName="is-active" className="btn btn-outline-light border-0 fw-bold fst-italic" aria-current="page" to="/" exact>Home</NavLink>
-=======
-                  <NavLink activeClassName="is-active" className="btn btn-outline-primary border-0 fw-normal text-decoration-italic" aria-current="page" to="/" exact><b>Home</b></NavLink>
->>>>>>> Stashed changes
                 </li>
 
                 <li className="nav-item">
                   <NavLink activeClassName="is-active" className="btn btn-outline-light border-0 fw-bold fst-italic" to="/StudentRegister">Student signup</NavLink>
                 </li>
                 <li className="nav-item">
-<<<<<<< Updated upstream
                   <NavLink activeClassName="is-active" className="btn btn-outline-light border-0 fw-bold fst-italic" to="/FacultyRegister">Faculty signup</NavLink>
                 </li>
               </ul>
               <div className="d-flex align-items-center" >
                 <NavLink activeClassName="is-active" className="btn btn-outline-light border-0 fw-bold fs-5 fst-italic" to='/login'>Log in <i className="bi bi-box-arrow-in-right"></i></NavLink>
                       
-=======
-                  <NavLink activeClassName="is-active" className="btn btn-outline-primary border-0 fw-normal text-decoration-italic" to="/FacultyRegister"><b>Faculty signup</b></NavLink>
-                </li>
-              </ul>
-              <div className="d-flex align-items-center" >
-                <NavLink activeClassName="is-active" to='/login'><button className="btn btn-light btn-outline-primary fw-bold" type="button"><b>Log in </b><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
-                  <path fillRule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z" />
-                  <path fillRule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
-                </svg></button></NavLink>
-
->>>>>>> Stashed changes
               </div>
             </div>
           </div>
